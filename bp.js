b13a230c6c552e31eb3775532671d2f7a927fdbd
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-06-05 13:34:29 -0500

    Anexo el js sufrio algunas modificaciones, reemplazalo en la carpeta js

---
+++ bp.js
@@ -0,0 +1,25 @@
+$(function(){
+	var visible = 0;
+	 $(".navbar-btn").click(function(){
+
+		if(visible===0){
+			$("#GRIDS").removeClass("col-md-offset-2");
+			$("#GRIDS").removeClass("col-xs-offset-4");
+			$("#GRIDS").removeClass("col-md-10");
+			$("#GRIDS").addClass("col-md-12");
+
+			$("#FILTRO").fadeOut();
+			visible = 1;
+		}
+		else{
+			$("#GRIDS").addClass("col-md-offset-2");
+			$("#GRIDS").addClass("col-xs-offset-4");
+			$("#GRIDS").addClass("col-md-10");
+			$("#GRIDS").removeClass("col-md-12");
+
+			$("#FILTRO").fadeIn();
+			visible = 0;
+		}
+
+	 });
+});
